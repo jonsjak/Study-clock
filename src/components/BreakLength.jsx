84e5e7b16bb4fc60e00bc61a8d435c448af42af9
Jonas Jakobson <jonas.jakobson@gmail.com>
2024-02-22 20:19:51 +0100

    added useEffect to reset break time

--- src/components/BreakLength.jsx
+++ src/components/BreakLength.jsx
@@ -4,20 +4,40 @@ import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import { LengthContainer } from "./styled_components/LengthContainer";
 import { TimerSpan } from "./styled_components/TimerSpan";
 import { StyledButton } from "./styled_components/StyledButton";
+import { useEffect, useState } from "react";
+import { resetThunk } from "../state/thunks/resetThunk";
 
 export const BreakLength = () => {
   const dispatch = useDispatch();
-  const breakDuration = useSelector(state => state.break.duration)
+  const breakDuration = useSelector(state => state.break.duration);
+  const [breakLength, setBreakLength] = useState(breakDuration);
+
+  useEffect(() => {
+    const breakIsUp = breakDuration === 0;
+    if (breakIsUp) {
+      setTimeout(() => setBreakLength(5), 1000)
+    }
+  }, [breakDuration, dispatch])
+
+  const handleIncrease = () => {
+    setBreakLength(breakLength + 1)
+    dispatch(increaseBreak())
+  }
+
+  const handleDecrease = () => {
+    setBreakLength(breakLength > 0 ? breakLength - 1 : breakLength);
+    dispatch(decreaseBreak());
+  }
 
   return (
     <LengthContainer>
       <h2 id="break-label">Break Length</h2>
       <TimerSpan>
-        <StyledButton onClick={() => dispatch(decreaseBreak())}>
+        <StyledButton onClick={() => handleDecrease()}>
           <FaArrowDown/>
         </StyledButton>
-        <p id="break-length">{breakDuration}</p>
-        <StyledButton onClick={() => dispatch(increaseBreak())}>
+        <p id="break-length">{breakLength}</p>
+        <StyledButton onClick={() => handleIncrease()}>
           <FaArrowUp />
         </StyledButton>
       </TimerSpan>
--- src/components/StudyLength.jsx
+++ src/components/StudyLength.jsx
@@ -4,40 +4,21 @@ import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import { LengthContainer } from './styled_components/LengthContainer';
 import { TimerSpan } from './styled_components/TimerSpan';
 import { StyledButton } from './styled_components/StyledButton';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { resetThunk } from "../state/thunks/resetThunk";
 
 export const StudyLength = () => {
   const studyDuration = useSelector(state => state.study.duration);
-  const [studyLength, setStudyLength] = useState(studyDuration)
+  const [studyLength, setStudyLength] = useState(studyDuration);
   const dispatch = useDispatch();
-  const isRunning = useSelector(state => state.study.isRunning)
-
+  const isRunning = useSelector(state => state.study.isRunning);
+  
+  /* ADD USEEFFECT TO RESET STUDY LENGHT WHEN BREAK TIME IS OVER */
+  
   //First makes sure to only handle changes when clock is NOT running
   //then checks if the local duration from the useState is equal to the redux state
   //then checks for increase or decrease (as long as studyLenght > 0)
   //finally (else) if the two values dont match, the local value is dispatched to state
-
-  /* const handleStudyLength = (increase) => {
-    if (!isRunning) {
-      let newDuration;
-      if (studyLength === studyDuration) {
-        switch (increase) {
-          case true:
-            newDuration = studyLength + 1;
-            break;
-          case false:
-            newDuration = studyLength > 0 ? studyLength - 1 : studyLength;
-            break;
-          default:
-            newDuration = studyLength;
-        }
-        setStudyLength(newDuration);
-        dispatch(increase ? increaseStudy() : decreaseStudy());
-      } else {
-        dispatch(setDuration(studyLength));
-      }
-    }
-  } */
 
   const handleIncrease = () => {
     if (!isRunning) {
